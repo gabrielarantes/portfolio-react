daf50bdcf84d1fbeda8f669874959157de537d6d
gabrielarantes <ghanicolau@gmail.com>
2018-10-17 22:25:33 -0300

    Updates

---
+++ precache-manifest.2d7c6c1d1bff049b9e8d964026e46fde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9706e54defff3e2f29b458310aa4f5cc",
+    "url": "/portfolio-react/static/media/img.9706e54d.jpg"
+  },
+  {
+    "revision": "ab8905455362987c8c6bbdf3b770ecf4",
+    "url": "/portfolio-react/static/media/blue.ab890545.png"
+  },
+  {
+    "revision": "9338e8a022e42c6bc02f",
+    "url": "/portfolio-react/static/js/runtime~main.9338e8a0.js"
+  },
+  {
+    "revision": "64c65bf487fe3f0a89df",
+    "url": "/portfolio-react/static/js/main.64c65bf4.chunk.js"
+  },
+  {
+    "revision": "7526e6bf6760043d3d9d",
+    "url": "/portfolio-react/static/js/1.7526e6bf.chunk.js"
+  },
+  {
+    "revision": "64c65bf487fe3f0a89df",
+    "url": "/portfolio-react/static/css/main.903b452a.chunk.css"
+  },
+  {
+    "revision": "da526f468917391db49e8c5ee82ac13f",
+    "url": "/portfolio-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09b68e007bf26a04d7f10caf11f0c371.js"
+  "/portfolio-react/precache-manifest.2d7c6c1d1bff049b9e8d964026e46fde.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
