7004b58fd27c24e83ca0e12b8b9ee17191464ccb
gabrielarantes <ghanicolau@gmail.com>
2018-10-17 21:55:26 -0300

    Updates

---
+++ precache-manifest.e1f13520925f0b2919343031bb26e7a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9706e54defff3e2f29b458310aa4f5cc",
+    "url": "/portfolio-react/static/media/img.9706e54d.jpg"
+  },
+  {
+    "revision": "ab8905455362987c8c6bbdf3b770ecf4",
+    "url": "/portfolio-react/static/media/blue.ab890545.png"
+  },
+  {
+    "revision": "9338e8a022e42c6bc02f",
+    "url": "/portfolio-react/static/js/runtime~main.9338e8a0.js"
+  },
+  {
+    "revision": "ba75090817c445fb3e22",
+    "url": "/portfolio-react/static/js/main.ba750908.chunk.js"
+  },
+  {
+    "revision": "7526e6bf6760043d3d9d",
+    "url": "/portfolio-react/static/js/1.7526e6bf.chunk.js"
+  },
+  {
+    "revision": "ba75090817c445fb3e22",
+    "url": "/portfolio-react/static/css/main.903b452a.chunk.css"
+  },
+  {
+    "revision": "81bb836506d404f9e65794417dbe0cf4",
+    "url": "/portfolio-react/index.html"
+  }
+];
